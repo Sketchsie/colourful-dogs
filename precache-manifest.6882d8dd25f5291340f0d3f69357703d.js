edf76e304bcd6588dc035ca78bff4ec4dcda9ccc
Prakhar Baveja <prakhar.baveja@gmail.com>
2019-07-08 20:56:42 +0530

    Updates

---
+++ precache-manifest.6882d8dd25f5291340f0d3f69357703d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "744553c986a14b3c5088a04fc2797458",
+    "url": "/colourful-dogs/index.html"
+  },
+  {
+    "revision": "8e94b937d5952a42df00",
+    "url": "/colourful-dogs/static/js/2.78098ed2.chunk.js"
+  },
+  {
+    "revision": "c44865bde221d3d6452d",
+    "url": "/colourful-dogs/static/js/main.a1426370.chunk.js"
+  },
+  {
+    "revision": "7b92a5f787d013f3efb1",
+    "url": "/colourful-dogs/static/js/runtime~main.475884da.js"
+  }
+]);
