7d35516edae7b895f3e68d5b103cb08931d72239
Prakhar Baveja <prakhar.baveja@gmail.com>
2019-07-08 17:20:09 +0530

    react palette added with image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { usePalette } from 'react-palette'
+import constants from './constants.js'
+function getRandomImage() {
+  return constants.images[Math.floor(Math.random() * constants.images.length)];
+}
 
 function App() {
+  const currImage = getRandomImage();
+  const { data, loading, error } = usePalette(currImage);
+  //console all colours hex codes
+  // !loading && console.log(data); 
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <div className="container-fluid">
+        {
+          !loading ? 
+          <div className="row">
+            <div className="text-center image-box">
+              <img src={currImage} width='200'/>
+            </div>
+            <div className="col-2 mcolumn" style={{ backgroundColor: data.vibrant}}>
+              <p>vibrant color</p>
+              <p>{data.vibrant}</p>
+            </div>
+            <div className="col-2 mcolumn" style={{ backgroundColor: data.darkMuted}}>
+              <p>dark muted color</p> 
+              <p>{data.darkMuted}</p>
+            </div>
+            <div className="col-2 mcolumn" style={{ backgroundColor: data. lightMute, color: 'darkslategray'}}>
+              <p>light muted color</p> 
+              <p>{data.lightMuted}</p>
+            </div>
+            <div className="col-2 mcolumn" style={{ backgroundColor: data.lightVibrant, color: 'darkslategray'}}>
+              <p>light vibrant color</p> 
+              <p>{data.lightVibrant}</p>
+            </div>
+            <div className="col-2 mcolumn" style={{ backgroundColor: data.muted}}>
+              <p>muted color</p> 
+              <p>{data.muted}</p>
+            </div>
+            <div className="col-2 mcolumn" style={{ backgroundColor: data.darkVibrant}}>
+              <p>dark vibrant color</p> 
+              <p>{data.vibrant}</p>
+            </div>
+          </div>
+          : <div className="row"><div className="col-12 mcolumn"><p>Loading...</p></div></div>
+        }
+        </div>
   );
 }
 
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+module.exports = Object.freeze({
+  // image: "http://cors-anywhere.herokuapp.com/https://pl.tedcdn.com/rss_feed_images/ted_talks_main_podcast/audio.png"
+  // image: "https://i1.sndcdn.com/avatars-000177412675-ff5g70-t500x500.jpg",
+  images: [
+  	'https://images.unsplash.com/photo-1537151672256-6caf2e9f8c95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=333&q=80',
+  	'https://images.unsplash.com/photo-1507146426996-ef05306b995a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80',
+  	'https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=360&q=80',
+  	'https://images.unsplash.com/photo-1554456854-55a089fd4cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  	'https://images.unsplash.com/photo-1507682520764-93440a60e9b5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=625&q=80'
+  ]
+});
