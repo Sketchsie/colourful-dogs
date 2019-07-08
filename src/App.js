f8bab1fc58922f40923fd233cae2c75782f0a800
Prakhar Baveja <prakhar.baveja@gmail.com>
2019-07-08 21:13:40 +0530

    responsive layout

--- src/App.js
+++ src/App.js
@@ -19,29 +19,29 @@ function App() {
           !loading ? 
           <div className="row">
             <div className="text-center image-box">
-              <img src={currImage} width='150'/>
+              <img src={currImage} className="img-fluid" style={{maxWidth: '150px'}}/>
             </div>
-            <div className="col-2 mcolumn" style={{ backgroundColor: data.vibrant}}>
+            <div className="col-6 col-sm-3 col-md-4 col-lg-2 mcolumn" style={{ backgroundColor: data.vibrant}}>
               <p>vibrant color</p>
               <p>{data.vibrant}</p>
             </div>
-            <div className="col-2 mcolumn" style={{ backgroundColor: data.darkMuted}}>
+            <div className="col-6 col-sm-3 col-md-4 col-lg-2 mcolumn" style={{ backgroundColor: data.darkMuted}}>
               <p>dark muted color</p> 
               <p>{data.darkMuted}</p>
             </div>
-            <div className="col-2 mcolumn" style={{ backgroundColor: data. lightMute, color: 'darkslategray'}}>
+            <div className="col-6 col-sm-3 col-md-4 col-lg-2 mcolumn" style={{ backgroundColor: data. lightMute, color: 'darkslategray'}}>
               <p>light muted color</p> 
               <p>{data.lightMuted}</p>
             </div>
-            <div className="col-2 mcolumn" style={{ backgroundColor: data.lightVibrant, color: 'darkslategray'}}>
+            <div className="col-6 col-sm-3 col-md-4 col-lg-2 mcolumn" style={{ backgroundColor: data.lightVibrant, color: 'darkslategray'}}>
               <p>light vibrant color</p> 
               <p>{data.lightVibrant}</p>
             </div>
-            <div className="col-2 mcolumn" style={{ backgroundColor: data.muted}}>
+            <div className="col-6 col-sm-3 col-md-4 col-lg-2 mcolumn" style={{ backgroundColor: data.muted}}>
               <p>muted color</p> 
               <p>{data.muted}</p>
             </div>
-            <div className="col-2 mcolumn" style={{ backgroundColor: data.darkVibrant}}>
+            <div className="col-6 col-sm-3 col-md-4 col-lg-2 mcolumn" style={{ backgroundColor: data.darkVibrant}}>
               <p>dark vibrant color</p> 
               <p>{data.vibrant}</p>
             </div>
